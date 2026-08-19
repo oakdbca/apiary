a5b0725b0b1bd1da689e0f995159b8274f928c95
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-08-19 16:35:40 +0800

    Lint offending management commands to begin with

--- disturbance/management/commands/send_compliance_reminder.py
+++ disturbance/management/commands/send_compliance_reminder.py
@@ -1,37 +1,43 @@
-from django.core.management.base import BaseCommand
-from django.utils import timezone
+import logging
+
 from django.conf import settings
-from disturbance.components.compliances.models import Compliance
+from django.core.management.base import BaseCommand
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 
-import logging
+from disturbance.components.compliances.models import Compliance
+
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-    help = 'Send notification emails for compliances which has past due dates, and also reminder notification emails for those that are within the daterange prior to due_date (eg. within 14 days of due date)'
+    help = "Send notification emails for compliances which has past due dates, and also reminder notification emails for those that are within the daterange prior to due_date (eg. within 14 days of due date)"
 
     def handle(self, *args, **options):
         try:
             user = EmailUser.objects.get(email=settings.CRON_EMAIL)
-        except:
-            user = EmailUser.objects.create(email=settings.CRON_EMAIL, password = '')
+        except EmailUser.DoesNotExist:
+            user = EmailUser.objects.create(email=settings.CRON_EMAIL, password="")
 
         errors = []
         updates = []
-        today = timezone.localtime(timezone.now()).date()
-        logger.info('Running command {}'.format(__name__))
-        for c in Compliance.objects.filter(processing_status='due').filter(approval__apiary_approval=True):
+
+        logger.info(f"Running command {__name__}")
+        for c in Compliance.objects.filter(processing_status="due").filter(approval__apiary_approval=True):
             try:
                 c.send_reminder(user)
                 c.save()
                 updates.append(c.lodgement_number)
             except Exception as e:
-                err_msg = 'Error sending Reminder Compliance'.format(c.lodgement_number)
-                logger.error('{}\n{}'.format(err_msg, str(e)))
+                err_msg = "Error sending Reminder Compliance"
+                logger.exception(f"{err_msg}\n{str(e)}")
                 errors.append(err_msg)
 
-        cmd_name = __name__.split('.')[-1].replace('_', ' ').upper()
-        err_str = '<strong style="color: red;">Errors: {}</strong>'.format(len(errors)) if len(errors)>0 else '<strong style="color: green;">Errors: 0</strong>'
-        msg = '<p>{} completed. {}. IDs updated: {}.</p>'.format(cmd_name, err_str, updates)
+        cmd_name = __name__.split(".")[-1].replace("_", " ").upper()
+        err_str = (
+            f'<strong style="color: red;">Errors: {len(errors)}</strong>'
+            if len(errors) > 0
+            else '<strong style="color: green;">Errors: 0</strong>'
+        )
+        msg = f"<p>{cmd_name} completed. {err_str}. IDs updated: {updates}.</p>"
         logger.info(msg)
-        print(msg) # will redirect to cron_tasks.log file, by the parent script
+        print(msg)  # will redirect to cron_tasks.log file, by the parent script
